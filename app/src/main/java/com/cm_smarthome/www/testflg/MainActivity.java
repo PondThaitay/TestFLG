6f6fb104a873d78a1235ee23afa4325f5e29a7c7
Jedsada Tiwongvorakul <pondthaitay@gmail.com>
2015-06-25 19:03:02 +0700

    just now

--- app/src/main/java/com/cm_smarthome/www/testflg/MainActivity.java
+++ app/src/main/java/com/cm_smarthome/www/testflg/MainActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
@@ -17,14 +20,20 @@ import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.facebook.widget.LoginButton.UserInfoChangedCallback;
+import com.facebook.widget.ProfilePictureView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -64,12 +73,13 @@ public class MainActivity extends FragmentActivity {
 
         setContentView(R.layout.activity_main);
 
-        mMap = ((SupportMapFragment)getSupportFragmentManager()
+        mMap = ((SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map)).getMap();
 
         mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
 
         userName = (TextView) findViewById(R.id.user_name);
+
         loginBtn = (LoginButton) findViewById(R.id.fb_login_button);
         loginBtn.setUserInfoChangedCallback(new UserInfoChangedCallback() {
             @Override
@@ -77,6 +87,9 @@ public class MainActivity extends FragmentActivity {
                 if (user != null) {
                     mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                     userName.setText("สวัสดี คุณ  " + user.getName());
+                    ProfilePictureView pictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
+                    pictureView.setProfileId(user.getId());
+                    Log.e("user_id", user.getId());
                 } else {
                     mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
                     userName.setText("You are not logged");
@@ -89,23 +102,21 @@ public class MainActivity extends FragmentActivity {
 
             @Override
             public void onClick(View view) {
-               try{
-                   Bundle params = new Bundle();
-                   params.putString("access_token", "YOUR ACCESS TOKEN");
-                   params.putString("place", "203682879660695");  // YOUR PLACE ID
-                   params.putString("message","I m here in this place");
-                   JSONObject coordinates = new JSONObject();
-                   coordinates.put("",LOCATION_SERVICE);
-                   coordinates.put("longitude", "YOUR LONGITUDE");
-                   params.putString("coordinates",coordinates.toString());
-                   params.putString("tags", "xxxx");//where xx indicates the User Id
-                   //String response = .request("me/checkins", params, "POST");
-                   //Log.d("Response", response);
-               }
-               catch (Exception e)
-               {
+                try {
+                    Bundle params = new Bundle();
+                    params.putString("access_token", "YOUR ACCESS TOKEN");
+                    params.putString("place", "203682879660695");  // YOUR PLACE ID
+                    params.putString("message", "I m here in this place");
+                    JSONObject coordinates = new JSONObject();
+                    coordinates.put("", LOCATION_SERVICE);
+                    coordinates.put("longitude", "YOUR LONGITUDE");
+                    params.putString("coordinates", coordinates.toString());
+                    params.putString("tags", "xxxx");//where xx indicates the User Id
+                    //String response = .request("me/checkins", params, "POST");
+                    //Log.d("Response", response);
+                } catch (Exception e) {
 
-               }
+                }
             }
         });
 
@@ -175,7 +186,7 @@ public class MainActivity extends FragmentActivity {
                     Session.getActiveSession(), img, new Request.Callback() {
                         @Override
                         public void onCompleted(Response response) {
-                            Toast.makeText(context,"Photo uploaded successfully",Toast.LENGTH_LONG).show();
+                            Toast.makeText(context, "Photo uploaded successfully", Toast.LENGTH_LONG).show();
 
 
                         }
@@ -194,7 +205,7 @@ public class MainActivity extends FragmentActivity {
                         @Override
                         public void onCompleted(Response response) {
                             if (response.getError() == null)
-                                Toast.makeText(context,"Status updated successfully",Toast.LENGTH_LONG).show();
+                                Toast.makeText(context, "Status updated successfully", Toast.LENGTH_LONG).show();
                         }
                     });
             request.executeAsync();
@@ -221,30 +232,30 @@ public class MainActivity extends FragmentActivity {
     @Override
     public void onResume() {
         super.onResume();
-        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         boolean isNetwork =
                 lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
         boolean isGPS =
                 lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        if(isNetwork) {
+        if (isNetwork) {
             lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER
                     , 5000, 10, listener);
             Location loc = lm.getLastKnownLocation(
                     LocationManager.NETWORK_PROVIDER);
-            if(loc != null) {
+            if (loc != null) {
                 lat = loc.getLatitude();
                 lng = loc.getLongitude();
             }
         }
 
-        if(isGPS) {
+        if (isGPS) {
             lm.requestLocationUpdates(LocationManager.GPS_PROVIDER
                     , 5000, 10, listener);
             Location loc = lm.getLastKnownLocation(
                     LocationManager.GPS_PROVIDER);
-            if(loc != null) {
+            if (loc != null) {
                 lat = loc.getLatitude();
                 lng = loc.getLongitude();
             }
@@ -278,4 +289,17 @@ public class MainActivity extends FragmentActivity {
         super.onSaveInstanceState(savedState);
         uiHelper.onSaveInstanceState(savedState);
     }
+
+   /* public Bitmap getImageProfileFacebook(String user_id) {
+        String imageURL;
+        Bitmap bitmap = null;
+        imageURL = "http://graph.facebook.com/" + user_id + "/picture?type=small";
+
+        try {
+            bitmap = BitmapFactory.decodeStream((InputStream) new URL(imageURL).getContent());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }*/
 }
